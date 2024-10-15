dd6151176574e47e8c8a75bc99b8eb5d7ad797cd
Max Maiers <maxmaiers@gmail.com>
2024-10-15 11:31:59 -0500

    added f string with ticker

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=Q9PQI5FO67EYWG2P'
+ticker = "IBM"
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey=Q9PQI5FO67EYWG2P"
 r = requests.get(url)
 data = r.json()
 
