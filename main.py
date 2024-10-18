121a5adc253ba617fae646d3925de8f89db10c94
Max Maiers <maxmaiers@gmail.com>
2024-10-18 11:59:40 -0500

    made some changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Q9PQI5FO67EYWG2P
 
 import requests
-
+from typing import List, Tuple, Callable, Any
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 ticker = "IBM"
 url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&interval=5min&apikey=Q9PQI5FO67EYWG2P"
@@ -11,19 +11,34 @@ data = r.json()
 
 print(data)
 
-def get_high(day: dict):
-    return day
+def get_open(day: str):
+    return data[day]["1. open"]
 
-def volume_on_day():
-    pass
+def get_high(day: str):
+    return data[day]["2. high"]
 
+def get_low(day: str):
+    return data[day]["3. low"]
+
+def get_close(day: str):
+    return data[day]["4. close"]
+
+def get_volume(day: str):
+    return data[day]["5. volume"]
+
+def change_by_range(day1: str, day2: str):
+    return abs((get_open(day1)) - (get_open(day2)))
+
+def buy_sell_hold():
+    currentDate = data["Meta Data"]["3. Last Refreshed"]
+    
 
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
-    (str.split("what was the volume on _"), volume_on_day),
-    (str.split("what was the high on _"), high_on_day),
-    (str.split("what was the low on _"), low_on_day),
-    (str.split("what was the open on _"), open_on_day),
-    (str.split("what was the close on _"), close_on_day),
+    (str.split("what was the volume on _"), get_volume),
+    (str.split("what was the high on _"), get_high),
+    (str.split("what was the low on _"), get_low),
+    (str.split("what was the open on _"), get_open),
+    (str.split("what was the close on _"), get_close),
     # note there are two valid patterns here two different ways to ask for the director
     # of a movie
     (str.split("what was the change between _ and _"), change_by_range),
